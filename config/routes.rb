6103f080c78f8cf78ceb340ccff28146e6c7e562
EC2 Default User <ec2-user@ip-172-31-33-145.ec2.internal>
2024-01-20 14:25:54 +0000

    [Add] 3-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  root 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
